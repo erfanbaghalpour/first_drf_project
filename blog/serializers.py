5731ef80ca98837ed5458af2bc519e4d621a7cf9
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2023-10-13 18:21:31 +0330

    use ModelSerializer

--- blog/serializers.py
+++ blog/serializers.py
@@ -8,11 +8,20 @@ class UserSerializer(serializers.Serializer):
     email = serializers.EmailField(max_length=190)
 
 
-class ArticleSerializer(serializers.Serializer):
-    id = serializers.IntegerField(required=False)
-    title = serializers.CharField()
-    text = serializers.CharField()
-    status = serializers.BooleanField(required=False)
+# class ArticleSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(required=False)
+#     title = serializers.CharField()
+#     text = serializers.CharField()
+#     status = serializers.BooleanField(required=False)
+#
+#     def create(self, validated_data):
+#         return Article.objects.create(**validated_data)
 
-    def create(self, validated_data):
-        return Article.objects.create(**validated_data)
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article()
+        fields = "__all__"
+        # also we can filter them by using the codes in below :
+        # fields = ("title", "text", ...)
+        # and if we want to remove a field we can use this :
+        # exclude = ("status", ...)
