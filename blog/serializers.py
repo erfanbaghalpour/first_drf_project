6228777b3f46f6df633bfaf218975dfb82302078
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2023-10-17 18:30:25 +0330

    Show comments

--- blog/serializers.py
+++ blog/serializers.py
@@ -33,32 +33,6 @@ class CheckTitle:
 #     def create(self, validated_data):
 #         return Article.objects.create(**validated_data)
 
-class ArticleSerializer(serializers.ModelSerializer):
-    status = serializers.BooleanField(write_only=True)
-
-    class Meta:
-        model = Article
-        fields = ("__all__")
-        validators = [
-            CheckTitle()
-        ]
-        # also we can filter them by using the codes in below :
-        # fields = ("title", "text", ...)
-        # and if we want to remove a field we can use this :
-        # exclude = ("status", ...)
-
-        read_only_fields = ["id"]
-
-    # def validate_title(self, value):
-    #     if value == "html":
-    #         raise serializers.ValidationError("You can choose html")
-    #     return value
-    # def validate(self, attrs):
-    #     if attrs['title'] == attrs['text']:
-    #         raise serializers.ValidationError("title and text can not be the same!")
-    #     return attrs
-
-
 class CommentSerializer(serializers.ModelSerializer):
     days_ago = serializers.SerializerMethodField()
     date = serializers.SerializerMethodField()
@@ -73,3 +47,34 @@ class CommentSerializer(serializers.ModelSerializer):
     def get_date(self, obj):
         date = JalaliDate(obj.date, locale="fa")
         return date.strftime('%c')
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    status = serializers.BooleanField(write_only=True)
+    comments = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Article
+        fields = ("__all__")
+        validators = [
+            CheckTitle()
+        ]
+        # also we can filter them by using the codes in below :
+        # fields = ("title", "text", ...)
+        # and if we want to remove a field we can use this :
+        # exclude = ("status", ...)
+
+        read_only_fields = ["id"]
+
+    def get_comments(self, obj):
+        serializer = CommentSerializer(instance=obj.comments.all(), many=True)
+        return serializer.data
+
+# def validate_title(self, value):
+#     if value == "html":
+#         raise serializers.ValidationError("You can choose html")
+#     return value
+# def validate(self, attrs):
+#     if attrs['title'] == attrs['text']:
+#         raise serializers.ValidationError("title and text can not be the same!")
+#     return attrs
